612c3827a70cffa30f9c4932e756e34422bcc199
dheerajcl <cldheeraj541@gmail.com>
2024-04-06 19:57:14 +0530

    Working on Landing page

--- src/app/components/MaxWidthWrapper.tsx
+++ src/app/components/MaxWidthWrapper.tsx
@@ -1,15 +1,15 @@
-import { stringify } from "querystring"
-import {ReactNode} from "react"
+import { cn } from "../lib/utils"
+import { ReactNode } from "react"
 
 const MaxWidthWrapper = ({
     className,
     children
-}:{
+}: {
     className?: string
     children: ReactNode
 }) => {
     return (
-        <div className="mx-auto w-full max-w-screen-x1 px-2.5 md:px-20">
+        <div className={cn('mx-auto w-full max-w-screen-x1 px-2.5 md:px-20', className)}>
             {children}
         </div>
     )  
