f0ce47ef4c5ecd67444fca9879fa71670b93bac9
dheerajcl <cldheeraj541@gmail.com>
2024-04-12 18:12:46 +0530

    Configuration Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (
-        config, 
-        {buildId, dev, isServer, defaultLoaders, webpack}) => {
-        config.resolve.alias.canvas = false;
-        config.resolve.alias.encoding = false;
-        return config;
-    },
-};
-
+        config,
+        { buildId, dev, isServer, defaultLoaders, webpack }
+      ) => {
+        config.resolve.alias.canvas = false
+        config.resolve.alias.encoding = false
+        return config
+      },
+}
 export default nextConfig;
