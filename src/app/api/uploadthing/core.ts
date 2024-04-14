3a6caeea5c390976330cf7b5fd89177b820f7483
dheerajcl <cldheeraj541@gmail.com>
2024-04-14 14:05:47 +0530

    Configuring uploadthing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
- 
+import { db } from "@/db";
 const f = createUploadthing();
  
 
@@ -15,7 +15,17 @@ export const ourFileRouter = {
 
         return {userId: user.id};
     })
-    .onUploadComplete(async ({ metadata, file }) => {}),
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: 'PROCESSING',
+        }
+      })
+    }),
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
